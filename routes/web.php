db4d0f5946ff04e5790b978efe5d5502c826230b
EnricoPiombini <piombini.enrico@gmail.com>
2022-08-02 15:50:33 +0200

    Route Comics created

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/comics', "ComicController@index")
+->name('comics.index');
