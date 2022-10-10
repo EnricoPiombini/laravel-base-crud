23694f1529574f77959816e94f797881535b7a53
EnricoPiombini <piombini.enrico@gmail.com>
2022-10-10 16:18:40 +0200

    routes changes

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
-Route::get('/comics',"ComicController@index")->name("comics.index");
+Route::get('/',"ComicController@index")->name("comics.index");
 Route::get('/comics/create',"ComicController@create")->name("comics.create");
 Route::get('/comics/{comic}',"ComicController@show")->name("comics.show");
 Route::get('/comics/{comic}/edit',"ComicController@edit")->name("comics.edit");
